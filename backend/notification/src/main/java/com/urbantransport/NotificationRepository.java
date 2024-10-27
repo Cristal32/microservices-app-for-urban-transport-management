672d7c23e7aa3a66751ab2977c2f6ea753281e34
fatibr19 <fatibraikat@gmail.com>
2024-10-27 21:13:36 +0100

    first commit for kafka & notification microservice

---
+++ backend/notification/src/main/java/com/urbantransport/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.urbantransport;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
